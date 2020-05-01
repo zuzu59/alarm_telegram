30bd254920752ee8b52d780500de51e49cdcc320
Christian Zufferey <christian@zufferey.com>
2020-05-01 17:58:13 +0200

    Refactorisé mon test d'envoi d'alarme ;-)

--- send_alarm_telegram.sh
+++ send_alarm_telegram.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Petit script pour envoyer une alarme sur Telegram
-#zf200501.1749
+#zf200501.1753
 
-source /keybase/private/zuzu59/alarm_telegram_zf.sh
+source /keybase/private/zuzu59/secrets_alarm_telegram_zf.sh
 
-export alarm_telegram_zf_message='zuzu_alert_1748'
+export alarm_telegram_zf_message='zuzu_alert-175350'
 
 curl -s --data chat_id=$alarm_telegram_zf_dest --data-urlencode text="$alarm_telegram_zf_message" "https://api.telegram.org/bot$alarm_telegram_zf_token/sendMessage" > /dev/null
 
