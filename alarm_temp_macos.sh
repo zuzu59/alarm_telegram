2835e89c48c362ebc630a69babe1dd3d8bc71916
Christian Zufferey <christian@zufferey.com>
2020-05-02 11:51:24 +0200

    Détecte les dépassements de température du CPU sur le MAC avec hystérèse de retour à la normale

---
+++ alarm_temp_macos.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#Petit script pour envoyer simplement un message sur Telegram quand la température du CPU du MAC dépasse la consigne
+#Il y a aussi une détection, avec hystérèse, du retour à la normale afin d'éviter les oscillations proches de la consigne
+#zf200502.1151
+
+#Source: 
+#https://debian-facile.org/doc:programmation:shells:page-man-bash-iii-les-operateurs-de-comparaison-numerique
+#https://www.system-linux.eu/index.php?post/2009/01/17/Operation-mathematique-simple-avec-Bash
+
+# lire la température sur MAC OS
+# il faut installer istats avec:
+# sudo gem install iStats
+# puis la lire avec
+# istats cpu temp  | awk '{print $3}' | sed "s/°C//g"
+
+# watch -n 1 './alarm_temp_macos.sh'
+# yes >/dev/null
+
+ZVAL=$(istats cpu temp  | awk '{print $3}' | sed "s/°C//g")
+ZCONSIGN_ON=57.0
+ZHYSTERESE=4.0
+ZCONSIGN_OFF=`echo "$ZCONSIGN_ON-$ZHYSTERESE" | bc -l`
+
+echo $ZVAL
+
+if (( $(echo "$ZVAL > $ZCONSIGN_ON" | bc -l) )) ; then
+  if [[ -f /tmp/toto.txt ]] ; then
+    echo " Alarm déjà envoyée"
+  else
+    touch /tmp/toto.txt
+    echo " Alarm, alarm, c'est trop chaud !"
+  fi
+else
+  if (( $(echo "$ZVAL < $ZCONSIGN_OFF" | bc -l) )) ; then
+    if [[ -f /tmp/toto.txt ]] ; then
+      rm /tmp/toto.txt
+      echo "retour à la normale !"
+    fi
+  fi
+  echo "c'est tout ok !"
+fi
+
+
--- test_alarm_numerique.sh
+++ test_alarm_numerique.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 #Petit script de test pour tester des déclenchements d'alarmes simples en bash
-#zf200501.1855
+#zf200502.1116
 
 #Source: https://debian-facile.org/doc:programmation:shells:page-man-bash-iii-les-operateurs-de-comparaison-numerique
 
-# lire la température sur MAC OS
-# il faut installer istats avec:
-# sudo gem install iStats
-# puis la lire avec
-# istats cpu temp  | awk '{print $3}' | sed "s/°C//g"
-
-# watch -n 1 './test_alarm_numerique.sh'
-# yes >/dev/null
-
-
-ZVAL=$(istats cpu temp  | awk '{print $3}' | sed "s/°C//g")
-ZCONSIGN=58.0
-
-
-
+ZVAL=55.7
+ZCONSIGN=60.0
 
 if (( $(echo "$ZVAL > $ZCONSIGN" | bc -l) )) ; then
   echo " Alarm alarm, c'est trop chaud ! "
 else
-  echo "c'est tout ok !"
+  echo "c'est ok !"
 fi
