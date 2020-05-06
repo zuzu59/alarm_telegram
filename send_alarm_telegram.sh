fa3e448793ae2fb48c7490dd9d3350ffb16a83b3
Christian Zufferey <christian@zufferey.com>
2020-05-06 09:10:50 +0200

    Refactorisé les secrets afin d'enlever le _zf_ et ajouté la possibilité d'avoir 2x id de destinataire. Modifié le send-alarm en conséquence

--- send_alarm_telegram.sh
+++ send_alarm_telegram.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Petit script pour envoyer une alarme sur Telegram
-#zf200504.1117
+#zf200506.0902
 
 #Usage:
 #./send_alarm_telegram.sh 'alarme feu'
@@ -10,5 +10,6 @@ source $(/usr/bin/dirname $0)/secrets_alarm_telegram.sh
 export alarm_telegram_zf_message='zuzu_alert-175350'
 
 #curl -s --data chat_id=$alarm_telegram_zf_dest --data-urlencode text="$alarm_telegram_zf_message" "https://api.telegram.org/bot$alarm_telegram_zf_token/sendMessage" > /dev/null
-/usr/bin/curl -s --data chat_id=$alarm_telegram_zf_dest --data-urlencode text="$1" "https://api.telegram.org/bot$alarm_telegram_zf_token/sendMessage" > /dev/null
+/usr/bin/curl -s --data chat_id=$alarm_telegram_dest1 --data-urlencode text="$1" "https://api.telegram.org/bot$alarm_telegram_token/sendMessage" > /dev/null
+/usr/bin/curl -s --data chat_id=$alarm_telegram_dest2 --data-urlencode text="$1" "https://api.telegram.org/bot$alarm_telegram_token/sendMessage" > /dev/null
 
