206be4621c69b5c526cdb697d434b47ee7e6bd26
Christian Zufferey <christian@zufferey.com>
2020-06-13 17:01:36 +0200

    Descendu encore la limite pour le NOC à 1.5GB

--- alarm_disque_noc-test.sh
+++ alarm_disque_noc-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Petit script pour envoyer simplement un message sur Telegram quand le disque devient pleins
 #Il y a aussi une détection, avec hystérèse, du retour à la normale afin d'éviter les oscillations proches de la consigne
-#zf200612.1334
+#zf200613.1701
 
 #Source: 
 #https://debian-facile.org/doc:programmation:shells:page-man-bash-iii-les-operateurs-de-comparaison-numerique
@@ -18,7 +18,7 @@
 #ZVAL=$(/usr/local/bin/istats cpu temp  | /usr/bin/awk '{print $3}' | /usr/bin/sed "s/°C//g")
 #ZVAL=`echo "scale=2;$(cat /sys/class/thermal/thermal_zone0/temp)/1000" | /usr/bin/bc -l`
 ZVAL=$(/bin/df -k |/bin/grep /dev/vda1 |/usr/bin/awk '{print $4}')
-ZCONSIGN_ON=1800000
+ZCONSIGN_ON=1500000
 ZHYSTERESE=100000
 ZCONSIGN_OFF=`echo "$ZCONSIGN_ON-$ZHYSTERESE" | /usr/bin/bc -l`
 ZFLAG=/tmp/alarm_disk.txt
