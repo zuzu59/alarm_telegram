dbe67bc7be61b98d686f947da94e76cac021ee48
Christian Zufferey <christian@zufferey.com>
2024-10-31 15:32:25 +0100

    change: changé la consigne alarme disque pour zuzu-test, passé de 9.5GB à 7.5GB

--- alarm_disque_zuzu-test.sh
+++ alarm_disque_zuzu-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Petit script pour envoyer simplement un message sur Telegram quand le disque devient pleins
 # Il y a aussi une détection, avec hystérèse, du retour à la normale afin d'éviter les oscillations proches de la consigne
-# zf200501.1701, zf231211.2051, zf240111.1905
+# zf200501.1701, zf231211.2051, zf241031.1531
 
 #Source: 
 #https://debian-facile.org/doc:programmation:shells:page-man-bash-iii-les-operateurs-de-comparaison-numerique
@@ -18,7 +18,7 @@
 #ZVAL=$(/usr/local/bin/istats cpu temp  | /usr/bin/awk '{print $3}' | /usr/bin/sed "s/°C//g")
 #ZVAL=`echo "scale=2;$(cat /sys/class/thermal/thermal_zone0/temp)/1000" | /usr/bin/bc -l`
 ZVAL=$(/bin/df -k |/bin/grep '/dev/vda1 ' |/usr/bin/awk '{print $4}')
-ZCONSIGN_ON=9500000
+ZCONSIGN_ON=7500000
 ZHYSTERESE=100000
 ZCONSIGN_OFF=`echo "$ZCONSIGN_ON-$ZHYSTERESE" | /usr/bin/bc -l`
 ZFLAG=/tmp/alarm_disk.txt
