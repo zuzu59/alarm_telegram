27bec753eaac6e8a684fbcc35b994f2fb557a98e
Christian Zufferey <christian@zufferey.com>
2025-03-29 18:22:10 +0100

    refact: mieux documenté pour la prochaine fois ;-)

--- alarm_disque_zuzu-test.sh
+++ alarm_disque_zuzu-test.sh
@@ -2,7 +2,7 @@
 # Petit script pour envoyer simplement un message sur Telegram quand le quota pour les images de la caméra est dépassé !
 # Ce n'est donc pas la place disque restante qui est sous moniteur mais la place occupée par les images, différent que d'habitude !
 # Il y a aussi une détection, avec hystérèse, du retour à la normale afin d'éviter les oscillations proches de la consigne
-# zf200501.1701, zf231211.2051, zf241031.1531, zf250329.1816
+# zf200501.1701, zf231211.2051, zf241031.1531, zf250329.1821
 
 #Source: 
 #https://debian-facile.org/doc:programmation:shells:page-man-bash-iii-les-operateurs-de-comparaison-numerique
@@ -21,6 +21,7 @@
 #ZVAL=$(/bin/df -k |/bin/grep '/dev/vda1 ' |/usr/bin/awk '{print $4}')
 ZVAL=$(/bin/du -b -d 0 /mnt/data/ | /usr/bin/awk '{print $1}')
 
+# Donc ce n'est PAS la place restante sur le disque mais le quota autorisé pour les images !
 ZCONSIGN_ON=15800000000
 ZHYSTERESE=1000
 ZCONSIGN_OFF=`echo "$ZCONSIGN_ON-$ZHYSTERESE" | /usr/bin/bc -l`
